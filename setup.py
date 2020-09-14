2d0ca387d493f2eb1cff3efcd55a8b44376de109
SudharsanAshokKumar <sudharsan7024@gmail.com>
2020-09-14 11:30:59 +0200

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a short analysis on COVID-19 data',
+    author='Sudharsan_Ashok_Kumar',
+    license='',
+)
